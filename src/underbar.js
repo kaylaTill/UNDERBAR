5dcc0112845b5e8cdf7a25d2074a0f6c6a885c97
kaylatillman <kaylatillman319@gmail.com>
2019-06-29 11:41:12 -0700

    UP TO ONCE

--- src/underbar.js
+++ src/underbar.js
@@ -292,11 +292,31 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    if (arguments.length > 1) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+        for (var key in source) {
+          obj[key] = source[key];
+        }
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    if (arguments.length > 1) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+        for (var key in source) {
+          if (!(key in obj)) {
+            obj[key] = source[key];
+          }
+        }
+      }
+    }
+    return obj;
   };
 
 
